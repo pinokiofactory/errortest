dd4051727b682cdfdb3f0ae792875052bd4398b9
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-15 22:20:14 -0500

    update

--- test.js
+++ test.js
@@ -4,13 +4,16 @@ module.exports = {
       "method": "shell.run",
       "params": {
         "venv": "env",
-        "message": "pip install gradio==5.5.0 uvicorn==0.13.4"
+        "message": "pip install gradio==5.5.0 uvicorn==0.13.4",
 //        "on": [{
-//          "event": "/error: subprocess-exited-with-error/i",
-//          "debug": true
-//        }]
-//        "error": [{
-//          "event": "/error: subprocess-exited-with-error/i"
+//          "event": "/error:/i",
+//          "break": true
+//        }, {
+//          "event": "/errno /i",
+//          "break": true
+//        }, {
+//          "event": "/error:.*triton/i",
+//          "break": false
 //        }]
       }
     },
